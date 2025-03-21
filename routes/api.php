5db7e18596b5b88221aeff16895f0711f867d1ba
Patrick0402 <bruno.patrick0402@gmail.com>
2025-03-21 11:40:29 -0300

    Feature: rota post para criar categoria

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(CategoriaController::class)->group(function(){
+    Route::get('/categoria', 'index');
+    Route::post('/categoria', 'store');
+});
