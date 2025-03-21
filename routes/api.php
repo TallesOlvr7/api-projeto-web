8862c6d7e74c3170410fe7065c220a51abc30439
Patrick0402 <bruno.patrick0402@gmail.com>
2025-03-21 13:03:30 -0300

    feat: crud completo de despesas

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\DespesaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,8 +10,16 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::controller(CategoriaController::class)->group(function(){
-    Route::get('/categoria', 'index'); // Rota para listar todas as categorias
+    Route::get('/categoria', 'index');
     Route::post('/categoria', 'store');
     Route::delete('/categoria/{categoria}', 'destroy');
-    Route::patch('/categoria/{categoria}', 'update'); // Rota para atualizar o nome de uma categoria
+    Route::patch('/categoria/{categoria}', 'update');
+});
+
+Route::controller(DespesaController::class)->group(function(){
+    Route::get('/despesa', 'index'); 
+    Route::post('/despesa', 'store');
+    Route::delete('/despesa/{despesa}', 'destroy');
+    Route::patch('/despesa/{despesa}', 'update'); 
+    Route::get('/despesa/relatorio/{mes}', 'relatorio');
 });
