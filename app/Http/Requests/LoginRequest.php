b9e3e27ff51d4980b0ecc06a7630f07077d276cb
TallesOlvr7 <tallesgabrieldeoliveirarodrig.com>
2025-03-21 10:41:14 -0300

    feat:making login feature

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UsuarioRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
 
     public function authorize(): bool
@@ -16,27 +16,19 @@ class UsuarioRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'usu_nome' => 'required|string|min:3|max:255',
             'usu_email' => 'required|email|unique:users,email',
             'usu_senha' => 'required|string|min:4',
         ];
     }
 
-    public function messages()
+    public function messages(): array
     {
         return [
-            'usu_nome.required' => 'O nome é obrigatório.',
-            'usu_nome.string' => 'O nome deve ser uma string válida.',
-            'usu_nome.min' => 'O nome deve ter pelo menos 3 caracteres.',
-            'usu_nome.max' => 'O nome não pode ter mais de 255 caracteres.',
-
             'usu_email.required' => 'O e-mail é obrigatório.',
             'usu_email.email' => 'O e-mail deve ser um endereço de e-mail válido.',
-            'usu_email.unique' => 'Este e-mail já está em uso.',
 
             'usu_senha.required' => 'A senha é obrigatória.',
             'usu_senha.string' => 'A senha deve ser uma string válida.',
-            'usu_senha.min' => 'A senha deve ter pelo menos 4 caracteres.',
         ];
     }
 }
